e2486501daa6afa6d723a4e7f03269cc02b62a1d
Ayush Sharma <160806563+ayuxharma@users.noreply.github.com>
2025-11-24 14:41:43 +0530

    Create Step 1.cpp

---
+++ .High Level Design/.Core Components of HLD/.Message Queue Implementation/Step 1.cpp
@@ -0,0 +1,9 @@
+//Step 1: Define the Message Structure:
+
+//Start by defining a structure for your messages. This structure should contain the necessary information for communication between different parts of your system.
+
+struct Message {
+    int messageType;
+    std::string payload;
+    // Add any other fields as needed
+};
